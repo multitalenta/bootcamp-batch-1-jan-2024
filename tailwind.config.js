4e821f6b2f7efa0a49f106dff583d6a267da316a
unknown <mhmmdhpdz@gmail.com>
2024-01-25 15:13:08 +0700

    portfolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#2dd4bf',
+        dark: '#1e293b',
+        secondary:'#64748b',
+      }
+    },
+  },
+  plugins: [],
+}
+

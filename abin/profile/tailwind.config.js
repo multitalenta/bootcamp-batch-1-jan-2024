e0f1abccf99c46283afea1893eebc16ec7fede65
Muhammad Hapidz <mhmmdhpdz@gmail.com>
2024-03-06 13:41:01 +0700

    First Portfolio
    Portfolio menggunakan Tailwind

---
+++ abin/profile/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode:'class',
+  theme: {
+    extend: {
+      colors:{
+        primary: '#2dd4bf',
+        dark: '#1e293b',
+        secondary:'#64748b',
+      }
+    },
+  },
+  plugins: [],
+}
+
